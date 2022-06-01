d0a80c63b66fb1fb037194bbe6dc942d96e43260
adam <adam.kassir@gmail.com>
2022-06-01 16:33:45 +0200

    premier commit quelques modifs de test

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app-bar
+        color="green"
+        dense
+        elevation="3"
+        rounded
+    >
+    <v-app-bar-nav-icon>
+    </v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+          <div class="d-flex align-center" style="width:200px">
+            <v-img
+                alt="mushroom img"
+                class="shrink mr-2"
+                contain
+                src="img/resize.webp"
+                transition="scale-transition"
+                width="100%"
+            />
+          </div>
+    
+    
+    </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar'
+  }
+</script>
